6139956e18ce769f66c754546bdfcabd91934571
weishunc5 <wilsonchen1993@hotmail.com>
2020-08-17 13:11:49 -0700

    Add files via upload

---
+++ group_sold_products_by_date.sql
@@ -0,0 +1,36 @@
+/*
+Write an SQL query to find for each date, the number of distinct products sold and their names.
+
+The sold-products names for each date should be sorted lexicographically. 
+
+Return the result table ordered by sell_date.
+
+The query result format is in the following example.
+
+Activities table:
++------------+-------------+
+| sell_date  | product     |
++------------+-------------+
+| 2020-05-30 | Headphone   |
+| 2020-06-01 | Pencil      |
+| 2020-06-02 | Mask        |
+| 2020-05-30 | Basketball  |
+| 2020-06-01 | Bible       |
+| 2020-06-02 | Mask        |
+| 2020-05-30 | T-Shirt     |
++------------+-------------+
+
+Result table:
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+*/
+
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date
+;
