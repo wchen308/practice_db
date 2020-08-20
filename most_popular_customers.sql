7797f5fa7bfad0cec99957b96f4916b23a9f49bf
weishunc5 <wilsonchen1993@hotmail.com>
2020-08-20 00:05:58 -0700

    Add files via upload

---
+++ most_popular_customers.sql
@@ -0,0 +1,29 @@
+/*
+Query the customer_number from the orders table for the customer who has placed the 
+largest number of orders.
+
+Sample Input
+
+| order_number | customer_number | order_date | required_date | shipped_date | status | comment |
+|--------------|-----------------|------------|---------------|--------------|--------|---------|
+| 1            | 1               | 2017-04-09 | 2017-04-13    | 2017-04-12   | Closed |         |
+| 2            | 2               | 2017-04-15 | 2017-04-20    | 2017-04-18   | Closed |         |
+| 3            | 3               | 2017-04-16 | 2017-04-25    | 2017-04-20   | Closed |         |
+| 4            | 3               | 2017-04-18 | 2017-04-28    | 2017-04-25   | Closed |         |
+Sample Output
+
+| customer_number |
+|-----------------|
+| 3               |
+*/
+
+/*
+logic:
+choose top value of count(*): ORDER BY, LIMIT 1
+*/
+
+SELECT customer_number
+FROM ORDERS
+GROUP BY customer_number
+ORDER BY count(customer_number) DESC
+LIMIT 1;
