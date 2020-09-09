cab0a0d57cf060eebd942330021cbbf4330fc7f6
weishunc5 <wilsonchen1993@hotmail.com>
2020-09-09 14:29:43 -0700

    Add files via upload

---
+++ multiple_acrticle_views_on_same_day.sql
@@ -0,0 +1,32 @@
+/*
+Write an SQL query to find all the people who viewed more than one article on the same date, sorted in ascending order by their id.
+
+The query result format is in the following example:
+
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 3          | 4         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+
+Result table:
++------+
+| id   |
++------+
+| 5    |
+| 6    |
++------+
+*/
+
+SELECT DISTINCT viewer_id AS id
+FROM Views
+GROUP BY viewer_id, view_date
+HAVING COUNT(DISTINCT article_id) > 1;
---
+++ self_article_views.sql
@@ -0,0 +1,31 @@
+/*
+Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.
+
+The query result format is in the following example:
+
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+
+Result table:
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+*/
+
+SELECT DISTINCT (viewer_id) AS id
+FROM Views
+WHERE viewer_id = author_id
+ORDER BY id;
