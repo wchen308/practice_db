1b468c11bdc3e9e4f48490c325ade5dae79c3c60
weishunc5 <wilsonchen1993@hotmail.com>
2020-09-16 22:13:18 -0700

    Add files via upload

---
+++ no_transaction_customers.sql
@@ -0,0 +1,53 @@
+/*
+Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+
+The query result format is in the following example:
+
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+
+Result table:
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
+*/
+
+/*
+find visits with no transaction
+count customer_id
+*/
+
+WITH no_transaction AS (SELECT visit_id, customer_id
+                        FROM Visits
+                        WHERE visit_id NOT IN (SELECT visit_id FROM Transactions))
+                        
+SELECT customer_id, COUNT(customer_id) AS count_no_trans
+FROM no_transaction
+GROUP BY customer_id;
