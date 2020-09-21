2152bb1f00e49bb70ca9d3d87e29e781ec367ed6
weishunc5 <wilsonchen1993@hotmail.com>
2020-09-21 13:26:59 -0700

    Add files via upload

---
+++ count_student_numbers_in_dept.sql
@@ -0,0 +1,56 @@
+/*
+Write a query to print the respective department name and number of students majoring in each department for all departments in the department table (even ones with no current students).
+
+Sort your results by descending number of students; if two or more departments have the same number of students, then sort those departments alphabetically by department name.
+
+The student is described as follow:
+
+| Column Name  | Type      |
+|--------------|-----------|
+| student_id   | Integer   |
+| student_name | String    |
+| gender       | Character |
+| dept_id      | Integer   |
+where student_id is the student's ID number, student_name is the student's name, gender is their gender, and dept_id is the department ID associated with their declared major.
+
+And the department table is described as below:
+
+| Column Name | Type    |
+|-------------|---------|
+| dept_id     | Integer |
+| dept_name   | String  |
+where dept_id is the department's ID number and dept_name is the department name.
+
+Here is an example input:
+student table:
+
+| student_id | student_name | gender | dept_id |
+|------------|--------------|--------|---------|
+| 1          | Jack         | M      | 1       |
+| 2          | Jane         | F      | 1       |
+| 3          | Mark         | M      | 2       |
+department table:
+
+| dept_id | dept_name   |
+|---------|-------------|
+| 1       | Engineering |
+| 2       | Science     |
+| 3       | Law         |
+The Output should be:
+
+| dept_name   | student_number |
+|-------------|----------------|
+| Engineering | 2              |
+| Science     | 1              |
+| Law         | 0              |
+*/
+
+WITH grouped_db AS (SELECT dept_id, COUNT(*) AS student_number
+                    FROM student
+                    GROUP BY dept_id)
+                    
+SELECT d.dept_name, IFNULL(g.student_number, 0) AS student_number
+FROM department AS d LEFT JOIN grouped_db AS g
+ON d.dept_id = g.dept_id
+ORDER BY student_number DESC, d.dept_name
+;
