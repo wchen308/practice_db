5a7b1dc3f8f0edcf2d6a65334e251a3975dc19bb
weishunc5 <wilsonchen1993@hotmail.com>
2020-11-05 04:11:07 -0800

    Add files via upload

---
+++ pct_of_registration.sql
@@ -0,0 +1,53 @@
+/*
+Write an SQL query to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Users table:
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+| 6       | Alice     |
+| 2       | Bob       |
+| 7       | Alex      |
++---------+-----------+
+
+Register table:
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+| 215        | 6       |
+| 209        | 2       |
+| 208        | 2       |
+| 210        | 6       |
+| 208        | 6       |
+| 209        | 7       |
+| 209        | 6       |
+| 215        | 7       |
+| 208        | 7       |
+| 210        | 2       |
+| 207        | 2       |
+| 210        | 7       |
++------------+---------+
+
+Result table:
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+| 208        | 100.0      |
+| 209        | 100.0      |
+| 210        | 100.0      |
+| 215        | 66.67      |
+| 207        | 33.33      |
++------------+------------+
+*/
+
+SELECT r.contest_id, ROUND(COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT u.user_id) FROM Users AS u) * 100, 2) AS percentage
+FROM Register AS r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id
+;
