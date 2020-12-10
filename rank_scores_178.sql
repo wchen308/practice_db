fadf29bd1e93e5e2d6977b5f7eb197800a57a96d
weishunc5 <wilsonchen1993@hotmail.com>
2020-12-09 17:24:58 -0800

    Add files via upload

---
+++ rank_scores_178.sql
@@ -0,0 +1,10 @@
+SELECT Score, DENSE_RANK() OVER (ORDER BY Score DESC) AS 'Rank'
+FROM Scores
+ORDER BY Score DESC;
+
+/*
+RANK: if same value, give same rank, skip rank number for the next one
+DENSE_RANK: will not skip rank number for the next one(no gap between rank number)
+ROW_NUMBER: if same value, rank according to row number
+make sure window function alias sometimes may be problematic in MySQL, use 'Rank' or rnk should be fine
+*/
