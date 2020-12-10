627fba51ef3a87a6b0699d429ba3225d38db4907
weishunc5 <wilsonchen1993@hotmail.com>
2020-12-09 17:25:35 -0800

    Delete rank_scores.sql

--- rank_scores.sql
+++
@@ -1,9 +0,0 @@
-SELECT Score, DENSE_RANK() OVER (ORDER BY Score DESC) AS Rank
-FROM Scores
-ORDER BY Score DESC;
-
-/*
-RANK: if same value, give same rank, skip rank number for the next one
-DENSE_RANK: will not skip rank number for the next one(no gap between rank number)
-ROW_NUMBER: if same value, rank according to row number
-*/
