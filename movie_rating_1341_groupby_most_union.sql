d60b52e023b5dbfebf53e5f691071f70ff3bf9aa
weishunc5 <wilsonchen1993@hotmail.com>
2020-12-10 16:46:14 -0800

    Add files via upload

---
+++ movie_rating_1341_groupby_most_union.sql
@@ -0,0 +1,80 @@
+/*
+Write the following SQL query:
+
+Find the name of the user who has rated the greatest number of movies.
+In case of a tie, return lexicographically smaller user name.
+
+Find the movie name with the highest average rating in February 2020.
+In case of a tie, return lexicographically smaller movie name.
+
+The query is returned in 2 rows, the query result format is in the following example:
+
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+
+Movie_Rating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+
+Result table:
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+*/
+
+(
+SELECT u.name AS results
+FROM Movie_Rating AS r LEFT JOIN Users AS u
+ON r.user_id = u.user_id
+GROUP BY r.user_id
+ORDER BY COUNT(*) DESC, u.name ASC
+LIMIT 1
+)
+
+UNION
+
+(
+SELECT m.title
+FROM Movie_Rating AS r LEFT JOIN Movies AS m
+ON r.movie_id = m.movie_id
+WHERE r.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY r.movie_id
+ORDER BY AVG(r.rating) DESC, m.title ASC
+LIMIT 1
+)
+;
+
+/*
+UNION VS UNION ALL:
+UNION will eliminate duplicate record
+*/
